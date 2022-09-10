3a2326294a6ceb4462a7603101437081e4848c60
dassshic <goryaeva.daria@gmail.com>
2022-09-10 18:25:51 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,31 @@ public class Main {
         if (age >= 18) {
             System.out.println("Поздравляем с совершеннолетием");
         }
-        if (age < 18) {
-                System.out.println("Нужно еще немного подождать");
+        else {
+            System.out.println("Нужно еще немного подождать");
         }
 
         System.out.println("task2");
-        int humanAge = 7;
+        int humanAge = 27;
         if (humanAge >= 7 && humanAge < 18) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (humanAge >= 18 && humanAge < 24) {
+        } else if (humanAge >= 18 && humanAge < 24) {
             System.out.println("человек уже закончил школу и может отправляться в университет");
         }
-        if (humanAge >= 24) {
+        else if (humanAge >= 24) {
             System.out.println("человек окончил университет и ему пора искать первую работу");
         }
 
         System.out.println("task3");
-        int passengers = 124;
+        int passengers = 5;
         if (passengers <= 60) {
             System.out.println("есть сидячее место в вагоне");
         }
-        if (passengers > 60 && passengers <= 102) {
+        else if (passengers > 60 && passengers <= 102) {
             System.out.println("есть стоячее место в вагоне");
         }
-        if (passengers > 102) {
+        else
+        {
             System.out.println("вагон уже полностью забит");
         }
     }
